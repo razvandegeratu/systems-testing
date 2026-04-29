20325cacfaec2267233e54b5c42335d401b6424b
Razvan Degeratu <razvandegeratu007@gmail.com>
2026-04-29 10:50:36 +0300

    Added Testing at push

--- laboratories/cicd-documentation/tree.py
+++ laboratories/cicd-documentation/tree.py
@@ -52,6 +52,8 @@ class Tree:
             return self._find(data, self.root)
         else:
             return None
+    
+
 
     def _find(self, data, node):
         if data == node.data:
@@ -75,11 +77,28 @@ class Tree:
             self._printInorderTree(node.right)
 
     def _printPreorderTree(self, node):
-        # TODO
         pass
 
-    def _printPostorderTree(self, node):
-        # TODO
-        pass
+
+# Unit tests pentru functia find
+def test_find_existing_data():
+    tree = Tree()
+    tree.add(5)
+    tree.add(3)
+    tree.add(7)
+    assert tree.find(3).data == 3, "Test failed: existing data not found"
+
+
+def test_find_non_existing_data():
+    tree = Tree()
+    tree.add(5)
+    tree.add(3)
+    tree.add(7)
+    assert tree.find(10) is None, "Test failed: non-existing data should return None"
+
+
+def test_find_empty_tree():
+    tree = Tree()
+    assert tree.find(5) is None, "Test failed: finding data in an empty tree should return None"
 
 
